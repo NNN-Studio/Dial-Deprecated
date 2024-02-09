b19e80db4e7950bc2605caf563ed92de4b169b6f
KrLite <68179735+KrLite@users.noreply.github.com>
2024-02-09 15:00:58 +0800

    preparing for segmented

---
+++ Dial/Views/ControllersView.swift
@@ -0,0 +1,22 @@
+//
+//  ControllersView.swift
+//  Dial
+//
+//  Created by KrLite on 2024/2/9.
+//
+
+import SwiftUI
+
+struct ControllersView: View {
+    var body: some View {
+        Text("Controllers")
+        .frame(
+            minWidth: 300, maxWidth: 300,
+            minHeight: 600, maxHeight: 600
+        )
+    }
+}
+
+#Preview {
+    ControllersView()
+}
---
+++ Dial/Views/DialView.swift
@@ -0,0 +1,22 @@
+//
+//  DialView.swift
+//  Dial
+//
+//  Created by KrLite on 2024/2/9.
+//
+
+import SwiftUI
+
+struct DialView: View {
+    var body: some View {
+        Text("Dial")
+            .frame(
+                minWidth: 300, maxWidth: 300,
+                minHeight: 300, maxHeight: 300
+            )
+    }
+}
+
+#Preview {
+    DialView()
+}
