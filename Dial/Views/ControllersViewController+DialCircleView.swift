4876a434d6a3c48a712af5e90dfb6641eae5d48b
KrLite <68179735+KrLite@users.noreply.github.com>
2024-02-17 20:20:49 +0800

    well, selectable

--- Dial/Views/ControllersViewController+DialCircleView.swift
+++ Dial/Views/ControllersViewController+DialCircleView.swift
@@ -33,7 +33,22 @@ extension ControllersViewController {
         let radiansMultiplier = 1.35
         
         let iconsView = NSView()
-        fillSubview(superview, iconsView)
+        iconsView.translatesAutoresizingMaskIntoConstraints = false
+        layerSubview(superview, iconsView)
+        
+        let buttonsView = NSView()
+        buttonsView.translatesAutoresizingMaskIntoConstraints = false
+        layerSubview(superview, buttonsView)
+        
+        func updateIconViews() {
+            for (index, iconView) in iconsView.subviews.compactMap({ $0 as? NSImageView }).enumerated() {
+                if index == Controllers.activatedControllers.firstIndex(where: { $0.id == Controllers.selectedController.id }) {
+                    iconView.contentTintColor = .controlAccentColor
+                } else {
+                    iconView.contentTintColor = nil
+                }
+            }
+        }
         
         Task { @MainActor in
             for await _ in Defaults.updates([.activatedControllerIDs, .shortcutsControllerSettings]) {
@@ -61,30 +76,39 @@ extension ControllersViewController {
                             iconView.centerYAnchor.constraint(equalTo: iconsView.centerYAnchor, constant: pos.y)
                         ])
                     }
+                
+                updateIconViews()
             }
         }
         
-        let buttonsView = NSView()
-        fillSubview(superview, buttonsView)
-        
         func updateButtons() {
-            
+            for (index, button) in buttonsView.subviews.compactMap({ $0 as? NSButton }).enumerated() {
+                if index == Controllers.activatedControllers.firstIndex(where: { $0.id == Controllers.selectedController.id }) {
+                    button.showsBorderOnlyWhileMouseInside = false
+                } else {
+                    button.showsBorderOnlyWhileMouseInside = true
+                }
+            }
         }
         
         Task { @MainActor in
             for await _ in Defaults.updates([.activatedControllerIDs, .shortcutsControllerSettings]) {
                 buttonsView.subviews.filter({ $0 is NSButton }).forEach({ $0.removeFromSuperview() })
                 
-                for index in 0...Controllers.activatedControllers.count {
+                for index in 0..<Controllers.activatedControllers.count {
                     let radians = self.getRadians(ofIndex: index)
-                    let radius = superview.frame.height / 4.0 * radiansMultiplier
+                    let radius = superview.frame.height / 4.0 * radiansMultiplier * 0.99
                     let pos = NSPoint(x: radius * sin(-radians - Double.pi), y: radius * cos(radians - Double.pi))
-                    let size = superview.frame.height / 4 * 0.32
+                    let size = superview.frame.height / 4 * 0.65
                     
                     let button = NSButton()
                     buttonsView.addSubview(button)
                     
                     button.translatesAutoresizingMaskIntoConstraints = false
+                    button.target = self
+                    button.action = #selector(self.dialCircleSelectController(_:))
+                    
+                    button.title = ""
                     button.bezelStyle = .badge
                     button.showsBorderOnlyWhileMouseInside = true
                     
@@ -99,16 +123,20 @@ extension ControllersViewController {
                 updateButtons()
             }
         }
+        
+        Task { @MainActor in
+            for await _ in Defaults.updates(.selectedControllerID) {
+                updateIconViews()
+                updateButtons()
+            }
+        }
     }
     
-    private func fillSubview(
+    private func layerSubview(
         _ superview: NSView,
         _ view: NSView
     ) {
         superview.addSubview(view)
-        
-        superview.translatesAutoresizingMaskIntoConstraints = false
-        view.translatesAutoresizingMaskIntoConstraints = false
         
         NSLayoutConstraint.activate([
             view.leadingAnchor.constraint(equalTo: superview.leadingAnchor),
@@ -122,6 +150,20 @@ extension ControllersViewController {
         ofIndex index: Int = Controllers.indexOf(Controllers.selectedController)!
     ) -> CGFloat {
         CGFloat(index % Defaults[.maxControllerCount]) / CGFloat(Defaults[.maxControllerCount]) * 2.0 * Double.pi
+    }
+    
+}
+
+extension ControllersViewController {
+    
+    @objc func dialCircleSelectController(_ sender: Any?) {
+        guard let button = sender as? NSButton else { return }
+        guard 
+            let index = button.superview?.subviews.filter({ $0 is NSButton }).firstIndex(of: button),
+            index < Controllers.activatedControllers.count
+        else { return }
+        
+        Controllers.selectedController = Controllers.activatedControllers[index]
     }
     
 }
